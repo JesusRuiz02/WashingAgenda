3dac83dc7710a58e2890dec31d09143f0bbc1081
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2025-12-09 00:08:19 -0600

    First View

---
+++ app/src/main/java/com/jesusruiz/washingagenda/views/Login.kt
@@ -0,0 +1,58 @@
+package com.jesusruiz.washingagenda.views
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.jesusruiz.washingagenda.R
+
+@Composable
+fun LoginView(){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column(Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.height(100.dp))
+        Image(modifier = Modifier.height(200.dp).width(200.dp).padding(20.dp),
+            painter = painterResource(id = R.drawable.washing_machine), contentDescription = "Login Icon"
+        )
+        OutlinedTextField(modifier = Modifier.padding(start = 30.dp, end = 30.dp).fillMaxWidth(),
+            value = email,
+            onValueChange = {email = it},
+            label = { Text(text = "Email",
+                style = LocalTextStyle.current.copy(color = Color.White)
+            )},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email ),
+            colors = OutlinedTextFieldDefaults.colors(focusedContainerColor = Color.Green,
+                unfocusedContainerColor = Color.Green)
+        )
+        OutlinedTextField(modifier = Modifier.padding(start = 30.dp, end = 30.dp).fillMaxWidth(),
+            value = password,
+            onValueChange = {password = it},
+            label = { Text(text = "Password",
+                style = LocalTextStyle.current.copy(color = Color.White)
+            )},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password ),
+            colors = OutlinedTextFieldDefaults.colors(focusedContainerColor = Color.Green,
+                unfocusedContainerColor = Color.Green))
+
+    }
+}
